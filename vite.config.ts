8290d6941c94a54160c17834623252a8153bd7a9
reportyao <22315113+reportyao@users.noreply.github.com>
2025-11-25 00:50:32 -0500

    fix: Add base tag to fix CORS and iframe path issues

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_')
 
   return {
-    base: 'https://seardao.org/',
     plugins: [react()],
     
     server: {
